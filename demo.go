58cfd9ee42e370b5e5604fb44c01d1a910a0be30
wenhao <840346763@qq.com>
2020-07-28 17:36:25 +0800

    接口示例

---
+++ demo.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type machine interface {
+	aircraft
+	car
+}
+
+type aircraft interface {
+	flight()
+}
+type car interface {
+	run()
+}
+
+type ferrari struct {
+	brand string
+}
+
+type tesla struct {
+	brand string
+}
+
+func (f ferrari) run() {
+	fmt.Printf("%s 速度100迈\n", f.brand)
+}
+
+func (t tesla) run() {
+	fmt.Printf("%s 速度200迈\n", t.brand)
+}
+
+func drive(c car) {
+	c.run()
+}
+
+func main() {
+	//f := ferrari{"法拉利"}
+	//t := tesla{"特斯拉电动车"}
+	//drive(f)
+	//drive(t)
+
+}
