63a771ab6eeb2fe2d85d45a7f2b5dc89fcc88ba3
wenhao <840346763@qq.com>
2020-07-31 10:54:27 +0800

    logs

--- log_test/testlog.go
+++ log_test/testlog.go
@@ -1,11 +1,10 @@
 package main
 
-import (
-	"go-study/mylogs"
-)
+import "go-study/mylogs"
 
 func main() {
-	log := mylogs.NewLog("error")
+
+	log := mylogs.NewLog("info")
 	for {
 		log.Info("这是一个 info 日志")
 		log.Error("这是一个 error 日志")
--- mylogs/consode.go
+++ mylogs/consode.go
@@ -5,46 +5,12 @@ import (
 	"time"
 )
 
-type LogLevel int
-
-const (
-	INFO LogLevel = iota
-	ERROR
-)
-
-// 日志结构体
-type Logger struct {
-	Level LogLevel
-}
-
-// 构造函数
-func NewLog(s string) Logger {
-	level, _ := parseLogLevel(s)
-	return Logger{
-		Level: level,
-	}
-}
-
-func parseLogLevel(s string) (LogLevel, error) {
-	switch s {
-	case "info":
-		return INFO, nil
-	case "error":
-		return ERROR, nil
-	default:
-		return ERROR, nil
-	}
-}
-
-func (l Logger) enable(level LogLevel) bool {
-	return l.Level == level
-}
-
 func (l Logger) Info(msg string) {
 	if l.enable(INFO) {
 		now := time.Now()
 		time := now.Format("2006-01-02 15:04:05")
-		fmt.Printf("[%s], %s\n", time, msg)
+		funcName, _, line := GetInfo(2)
+		fmt.Printf("[%s], [%s], [%s, %d] \n", time, msg, funcName, line)
 	}
 }
 
