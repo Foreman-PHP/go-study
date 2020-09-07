2a88681a4da67afae5ba4c8026904cde5f14d2d5
wenhao <840346763@qq.com>
2020-09-07 11:38:38 +0800

    day4

--- day02/slice/main.go
+++ day02/slice/main.go
@@ -12,33 +12,33 @@ import "fmt"
 func main() {
 
 	// 声明切片
-	//var s1 []int
-	//var s2 []string
-	//fmt.Println(s1, s2)
-	//fmt.Println(s1 == nil) // true 还没初始化 没有在内存中开辟空间
-	//fmt.Println(s2 == nil) // true
-	//
-	//// 初始化
-	//s1 = []int{1, 2, 3}
-	//s2 = []string{"学校", "校训", "xx"}
-	//fmt.Println(s1, s2)
-	//fmt.Println(s1 == nil) // false 已初始化
-	//fmt.Println(s2 == nil)
-	//
-	//// 长度和容量
-	//fmt.Printf("len(s1):%d cap(s1):%d\n", len(s1), cap(s1))
-	//fmt.Printf("len(s2):%d cap(s2):%d\n", len(s2), cap(s2))
-	//
-	//// 2. 由数组得到切片
-	//a1 := [...]int{1, 2, 3, 7, 8, 12, 31}
-	//s3 := a1[0:4] // 基于数组切割, 左包含右不不包含
-	//fmt.Println(s3)
-	//s4 := a1[:4] // 相当于 a1[0:4]
-	//s5 := a1[3:] // 相当于 a1[3:len(a1)] 到最后一位
-	//s6 := a1[:]  // 相当于 a1[0:len(a1)] 到最后一位
-	//fmt.Println(s4, s5, s6)
-	//
-	//fmt.Printf("len(s4):%d cap(s4):%d\n", len(s4), cap(s4)) // len(s4):4 cap(s4):7 切片的容量是底层数组从切片的第一个元素到最后一个元素的数量
+	var s1 []int
+	var s2 []string
+	fmt.Println(s1, s2)
+	fmt.Println(s1 == nil) // true 还没初始化 没有在内存中开辟空间
+	fmt.Println(s2 == nil) // true
+
+	// 初始化
+	s1 = []int{1, 2, 3}
+	s2 = []string{"学校", "校训", "xx"}
+	fmt.Println(s1, s2)
+	fmt.Println(s1 == nil) // false 已初始化
+	fmt.Println(s2 == nil)
+
+	// 长度和容量
+	fmt.Printf("len(s1):%d cap(s1):%d\n", len(s1), cap(s1))
+	fmt.Printf("len(s2):%d cap(s2):%d\n", len(s2), cap(s2))
+
+	// 2. 由数组得到切片
+	a1 := [...]int{1, 2, 3, 7, 8, 12, 31}
+	s3 := a1[0:4] // 基于数组切割, 左包含右不不包含
+	fmt.Println(s3)
+	s4 := a1[:4] // 相当于 a1[0:4]
+	s5 := a1[3:] // 相当于 a1[3:len(a1)] 到最后一位
+	s6 := a1[:]  // 相当于 a1[0:len(a1)] 到最后一位
+	fmt.Println(s4, s5, s6)
+
+	fmt.Printf("len(s4):%d cap(s4):%d\n", len(s4), cap(s4)) // len(s4):4 cap(s4):7 切片的容量是底层数组从切片的第一个元素到最后一个元素的数量
 
 	// 使用make函数创建切片
 	s7 := make([]int, 5, 10) // 切片的长度是5个长度 容量是10
@@ -57,5 +57,4 @@ func main() {
 	// append()追加元素, 原来的底层数组放不下的时候 Go底层就会把底层数组换一个
 	s9 = append(s9, "欢迎你")
 	fmt.Println(s9)
-
 }
